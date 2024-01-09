97e316054345855edf8dee1eb186fccbde75a13e
MouadRabihi07 <mouad_rabihi@hotmail.fr>
2024-01-09 11:36:21 +0100

    some changes

--- src/app/components/recent-transfers/recent-transfers.component.ts
+++ src/app/components/recent-transfers/recent-transfers.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Transfer } from '../../interfaces/transfer';
+import { TransferService } from '../../services/transfer.service';
 
 @Component({
   selector: 'app-recent-transfers',
@@ -9,7 +11,19 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './recent-transfers.component.html',
   styleUrl: './recent-transfers.component.css'
 })
-export class RecentTransfersComponent {
+export class RecentTransfersComponent implements OnInit {
   faTrash = faTrash;
+
+  transferService : TransferService = inject(TransferService); 
+
+
+  transfers ?: Transfer[];
+
+  ngOnInit(): void {
+
+    
+  }
+
+
 
 }
